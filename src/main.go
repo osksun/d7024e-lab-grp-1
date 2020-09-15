e077a88f641e67ff0763253bf599f857c5ef894d
MarqusJonsson <markusjonsson1997@gmail.com>
2020-09-15 17:00:31 +0200

    Update main.go

--- src/main.go
+++ src/main.go
@@ -6,9 +6,17 @@ import (
 
 func main() {
 	// Create nodes
-	node1 := d7024e.NewNode("localhost:8000")
-	node2 := d7024e.NewNode("localhost:8001")
+	node1 := d7024e.NewNode("localhost:8001")
+	node2 := d7024e.NewNode("localhost:8002")
+	node3 := d7024e.NewNode("localhost:8003")
+	// Create connections
+	node1.AddContact(node2.Contact())
+	node2.AddContact(node1.Contact())
+	node2.AddContact(node3.Contact())
 	// Spinup nodes
-	go node1.SpinupNode(node2.Contact, node2.Contact)
-	node2.SpinupNode(nil, nil)
+	go node1.SpinupNode(node3.Contact(), node2.Contact()) // Target, Receiver
+	go node2.SpinupNode(nil, nil)
+	go node3.SpinupNode(nil, nil)
+	// Infinite loop to prevent program from exiting
+	for {}
 }

d05b78120e2b66be95f7509f30a14ef5636bfffe
MarqusJonsson <markusjonsson1997@gmail.com>
2020-09-21 18:25:01 +0200

    Create cmd-exit.go

---
+++ src/cli/cmd-exit.go
@@ -0,0 +1,17 @@
+package cli
+
+import (
+	"os"
+)
+
+// Exit command for the cli
+func Exit() Cmd{
+	return Cmd{
+		triggers: []string{"exit", "quit", "terminate"},
+		description: "Exits the command line interface",
+		usage: "",
+		action: func(cli *Cli, args ...string) {
+			os.Exit(0)
+		},
+	}
+}

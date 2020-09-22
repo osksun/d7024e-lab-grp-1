527759f96e68effc06b5440117870cfe1a8b4488
MarqusJonsson <markusjonsson1997@gmail.com>
2020-09-22 10:28:42 +0200

    Add usage info

--- src/cli/cmd-help.go
+++ src/cli/cmd-help.go
@@ -10,7 +10,7 @@ func Help() Cmd{
 	return Cmd{
 		triggers: []string{"help", "h"},
 		description: "Shows list of available commands",
-		usage: "",
+		usage: "\"help\", \"h\"",
 		action: func(cli *Cli, args ...string) {
 			fmt.Printf("%-30s| %-40s| %-40s\n", "Name", "Description", "Usage")
 			for i := 0; i < len(cli.cmds); i++ {

1ed0022819f953fdba78afdd34a777ec0d580a3e
MarqusJonsson <markusjonsson1997@gmail.com>
2020-09-29 17:56:15 +0200

    Add unit test for cmd-exit

--- src/d7024e/cmd-exit.go
+++ src/d7024e/cmd-exit.go
@@ -10,8 +10,9 @@ func Exit() Cmd{
 		triggers: []string{"exit", "quit", "terminate"},
 		description: "Terminates the node",
 		usage: "\"exit\", \"quit\", \"terminate\"",
-		action: func(cli *Cli, args ...string) {
+		action: func(cli *Cli, args ...string) string {
 			os.Exit(0)
+			return "" // This will never be executed since we're exiting before
 		},
 	}
 }

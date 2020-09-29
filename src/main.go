c74a57d0458ffd3da5c4cf3b50665fabc9008349
MarqusJonsson <markusjonsson1997@gmail.com>
2020-09-29 11:15:08 +0200

    Update main.go

--- src/main.go
+++ src/main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	/*"math/rand"
-	"strconv"*/
 	"fmt"
+	"math/rand"
+	"strconv"
 	"./d7024e"
 )
 
@@ -16,26 +16,23 @@ func leftPad(str string, pad rune, lenght int) string {
 
 func main() {
 	// Create nodes
-
+	/*
 	node1 := d7024e.NewNode("localhost:8000")
 	node2 := d7024e.NewNode("localhost:8001")
 	node3 := d7024e.NewNode("localhost:8002")
 	node4 := d7024e.NewNode("localhost:8003")
-	node5 := d7024e.NewNode("localhost:8004")
 
 	node1.AddContact(node2.Contact())
 	node2.AddContact(node3.Contact())
+	node3.AddContact(node4.Contact())
 	node4.AddContact(node1.Contact())
-	node3.AddContact(node5.Contact())
-	node5.AddContact(node3.Contact())
 
 	go node2.SpinupNode(nil)
 	go node3.SpinupNode(nil)
 	go node4.SpinupNode(nil)
-	go node5.SpinupNode(nil)
-	node1.SpinupNode(nil)
-
-
+	node1.SpinupNode(node4.Contact())
+	*/
+	/*
 	var n4Buckets = node4.Rt().Buckets()
 	nConnections := 0
 	for i := 0; i < len(n4Buckets); i++ {
@@ -50,9 +47,9 @@ func main() {
 	for i := 0; i < len(n4Buckets); i++ {
 		nConnections += n4Buckets[i].Len()
 	}
-	fmt.Println("node4 connections after: ", nConnections)
+	fmt.Println("node4 connections after: ", nConnections)*/
 
-	/*const nNodes = 500
+	const nNodes = 500
 	var nodes [nNodes]*d7024e.Node
 	for i := 0; i < nNodes; i++ {
 		nodes[i] = d7024e.NewNode("localhost:" + strconv.Itoa(i+10000))
@@ -86,5 +83,4 @@ func main() {
 
 	targetContact := nodes[125].Contact()
 	nodes[0].SpinupNode(targetContact)
-	for {}
 }

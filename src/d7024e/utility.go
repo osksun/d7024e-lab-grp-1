978710c4166c4d7f941217fd4a8e33c2f15f4177
MarqusJonsson <markusjonsson1997@gmail.com>
2020-10-03 17:44:32 +0200

    Move function MinInt to utility.go
    Co-Authored-By: osksun <37369260+osksun@users.noreply.github.com>

--- src/d7024e/utility.go
+++ src/d7024e/utility.go
@@ -6,3 +6,16 @@ func leftPad(str string, pad rune, lenght int) string {
 	}
 	return str
 }
+
+// MinInt takes an arbitrary number of integers as in put and returns the smallest one
+func MinInt(vars ...int) int {
+	min := vars[0]
+
+	for _, i := range vars {
+		if min > i {
+			min = i
+		}
+	}
+
+	return min
+}

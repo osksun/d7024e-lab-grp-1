e7fd94ff5fc86ce967f70dd43d84f701c56ff7c5
MarqusJonsson <markusjonsson1997@gmail.com>
2020-10-04 16:23:57 +0200

    Reimplement LookupData
    Co-Authored-By: osksun <37369260+osksun@users.noreply.github.com>

--- src/d7024e/kademlia.go
+++ src/d7024e/kademlia.go
@@ -1,7 +1,7 @@
 package d7024e
 
 import (
-	//"encoding/hex"
+	"encoding/hex"
 	"fmt"
 	"time"
 )
@@ -17,7 +17,7 @@ type Kademlia struct {
 
 const k = 20
 const alpha = 3
-const maxRoundTime = 10 * time.Second
+const maxRoundTime = 3 * time.Second
 
 // NewKademlia Constructor function for Kademlia class
 func NewKademlia(rt *RoutingTable, ht *ValueHashtable) *Kademlia {
@@ -96,37 +96,72 @@ func (kademlia *Kademlia) JoinNetwork(address string, findNodeRequestChannel cha
 	// First send a FIND_NODE RPC call to the bootstrap node with self as target, the ID of the bootstrap node should not matter since it is not used when performing the lookup
 	findNodeResponseChannel := make(chan findNodeResponse)
 	findNodeRequestChannel <- findNodeRequest{kademlia.rt.me, NewContact(NewRandomKademliaID(), address), findNodeResponseChannel}
-	RPCResult := <- findNodeResponseChannel
+	findNodeResponse := <- findNodeResponseChannel
 	// Add each of the returned contacts from the bootstrap node to the joining node's buckets
 	for i := 0; i < len(findNodeResponse.contacts); i++ {
 		kademlia.rt.AddContact(findNodeResponse.contacts[i])
 	}
 	// Perform an refresh by executing a lookup with a random random ID as target which is not the same as the joining node's or it's neighbour
 	refreshContact := NewContact(kademlia.rt.me.ID.NewKademliaIDWithinRange(), "") // Probably not necessary to check for the collisions of the new random ID due to the large ID "space"
-	kademlia.LookupContact(refreshContact, findNodeRequestChannel)
+	kademlia.LookupContact(refreshContact, kademlia.k, findNodeRequestChannel)
 }
 
-/*
-func (kademlia *Kademlia) LookupData(hash [HashSize]byte) []byte {
+func (kademlia *Kademlia) LookupData(hash [HashSize]byte, findDataRequestChannel chan findDataRequest) []byte {
 	start := time.Now()
-	data := kademlia.net.ht.Get(hash)
+	data := kademlia.ht.Get(hash)
+	// Searching node already has hash in hashtable (Exit condition)
 	if data == nil {
-		c1 := make(chan []byte)
-		var initiatorList []Contact
-		kID, _ := NewKademliaID(hex.EncodeToString(hash[:]))
-		target := NewContact(kID, "")
-		initiatorList = kademlia.rt.FindClosestContacts(target.ID, kademlia.alpha)
-		for i := 0; i < MinInt(kademlia.alpha, len(initiatorList)); i++ {
-			go kademlia.goFindData(hash, &initiatorList[i], c1)
+		kademliaID, _ := NewKademliaID(hex.EncodeToString(hash[:]))
+		target := NewContact(kademliaID, "")
+		// Add the node who performs the lookup to the shortlist and set it to queried so it does not query itself
+		shortlist := NewContactCandidates([]Contact{*kademlia.rt.me})
+		shortlist.contacts[0].CalcDistance(target.ID)
+		shortlist.contacts[0].queried = true
+		// Find alpha closest contacts of own buckets
+		shortlist.Append(kademlia.rt.FindClosestContacts(target.ID, kademlia.alpha))
+		// Remove any potential duplicates and sort by distance to target (if FindClosestContacts returned the node who is performing the lookup there would be a duplicate)
+		shortlist.RemoveDuplicates()
+		shortlist.Sort()
+		// Note closest contact
+		closestContact := &shortlist.contacts[0]
+		numQueriedContacts := 0
+		queryNewContacts := true
+		// If we have queried k contacts the lookup is done (Exit condition)
+		for numQueriedContacts < kademlia.k {
+			// Find contacts to query that has not already been queried
+			currentQueryContacts := kademlia.findQueryContacts(shortlist)
+			// If there are no contacts to be queried the lookup is done (Exit condition)
+			if len(currentQueryContacts) <= 0 {
+				break
+			}
+			findDataResponseChannel := make(chan findDataResponse, len(currentQueryContacts))
+			roundStartTime := time.Now()
+			kademlia.sendFindDataRequests(hash, target, currentQueryContacts, findDataRequestChannel, findDataResponseChannel)
+			numResponses := 0
+			data, numResponses = kademlia.receiveFindDataResponses(&currentQueryContacts, shortlist, roundStartTime, queryNewContacts, findDataResponseChannel)
+			if data != nil {
+				// Data was found (Exit condition)
+				break
+			}
+			numQueriedContacts += numResponses
+			// Remove contacts from shortlist that hasn't responded yet
+			shortlist.removeContacts(currentQueryContacts)
+			// If a duplicate contact of an already queried contact has been added, the duplicate will most likely
+			// have the queried variable set to false. This should not be a problem since we remove every duplicate
+			// of every contact expect the first which should be the one with the queried variable set to true if it
+			// has been queried already
+			shortlist.RemoveDuplicates()
+			shortlist.Sort()
+			newCountClosestContact := &shortlist.contacts[0]
+			// If a cycle doesn't find a closer contact we don't want new found contacts to be queried
+			if !newCountClosestContact.Distance.Less(closestContact.Distance) {
+				queryNewContacts = false
+			}
 		}
-		data = <-c1
 	}
 	fmt.Println("Lookup data took", time.Since(start))
 	return data
 }
-*/
-/*
-func (kademlia *Kademlia) Store(filename []byte, data []byte) [HashSize]byte {
 	start := time.Now()
 	hash := Hash(filename)
 	kID, _ := NewKademliaID(hex.EncodeToString(hash[:]))
@@ -222,12 +257,44 @@ func (kademlia *Kademlia) receiveFindNodeResponses(queryContacts *[]*Contact, sh
 	}
 	return responsesReceived
 }
+
+func (kademlia *Kademlia) sendFindDataRequests(hash [HashSize]byte, target *Contact, queryContacts []*Contact, findDataRequestChannel chan findDataRequest, findDataResponseChannel chan findDataResponse) {
+	for i := 0; i < MinInt(alpha, len(queryContacts)); i++ {
+		findDataRequestChannel <- findDataRequest{hash, target, queryContacts[i], findDataResponseChannel}
+	}
+}
+
+func (kademlia *Kademlia) receiveFindDataResponses(queryContacts *[]*Contact, shortlist *ContactCandidates, roundStartTime time.Time, queryNewContacts bool, findDataResponseChannel chan findDataResponse) ([]byte, int) {
+	responsesReceived := 0
+	expectedResponses := len(*queryContacts)
+	// Wait either for kademlia.maxRoundTime or until we have gotten all expected responses depending on
+	// which is the quickest and handle every response we get
+	for time.Since(roundStartTime) < kademlia.maxRoundTime && responsesReceived < expectedResponses {
+		select {
+		case findDataResponse, ok := <- findDataResponseChannel:
+			if ok {
+				responsesReceived++
+				if findDataResponse.data != nil {
+					// Data was found (Exit condition)
+					return findDataResponse.data, responsesReceived
 				}
+				// TODO handle replies later than maxRoundTime
+				findDataResponse.sender.queried = true
+				if !queryNewContacts {
+					// Since we don't want to query any of the new contacts we set their queried variable to true
+					for i := 0; i < len(findDataResponse.contacts); i++ {
+						findDataResponse.contacts[i].queried = true
+					}
+				}
+				shortlist.Append(findDataResponse.contacts)
+				// Since we got a response we remove the responder from the currentQueryContacts
+				remaininqQueryContacts := removeContact(*queryContacts, findDataResponse.sender.ID)
+				(*queryContacts) = remaininqQueryContacts
 			} else {
-				flag = false
+				fmt.Println("\"findDataResponseChannel\" has been closed!")
 			}
+		default:
 		}
 	}
-	channel <- data
+	return nil, responsesReceived
 }
-*/

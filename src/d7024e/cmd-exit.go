fe3036a296fb540dcc6a5f7b9aa0abfa3e330f8d
MarqusJonsson <markusjonsson1997@gmail.com>
2020-10-04 16:08:15 +0200

    Add additional triggers
    Co-Authored-By: osksun <37369260+osksun@users.noreply.github.com>

--- src/d7024e/cmd-exit.go
+++ src/d7024e/cmd-exit.go
@@ -7,7 +7,7 @@ import (
 // Exit command for the cli
 func Exit() Cmd{
 	return Cmd{
-		triggers: []string{"exit", "quit", "terminate"},
+		triggers: []string{"exit", "quit", "terminate", "e", "q", "t"},
 		description: "Terminates the node",
 		usage: "\"exit\", \"quit\", \"terminate\"",
 		action: func(cli *Cli, args ...string) string {

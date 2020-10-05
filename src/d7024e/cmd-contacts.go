5fb8b9ec93801c9c5d5a75f3096c66bc58d7c0be
MarqusJonsson <markusjonsson1997@gmail.com>
2020-10-05 18:01:08 +0200

    Beautify output string
    Co-Authored-By: osksun <37369260+osksun@users.noreply.github.com>
    Co-Authored-By: Bringers <42640982+Bringers@users.noreply.github.com>

--- src/d7024e/cmd-contacts.go
+++ src/d7024e/cmd-contacts.go
@@ -12,13 +12,13 @@ func Contacts() Cmd{
 		usage: "\"contacts\", \"c\"",
 		action: func(cli *Cli, args ...string) string {
 			buckets := cli.node.rt.buckets
-			contactListStr := "\t| My address\t\t| My ID\n" + cli.node.contact.Address + "\t\t| " + cli.node.contact.ID.String() + "\nContact List\n"
-			contactListStr += "B_ID\t| Address\t\t| K_ID\t\t\t\t\t| Distance\n"
+			contactListStr := "\t| My address\t\t| My ID\n\t| " + cli.node.contact.Address + "\t| " + cli.node.contact.ID.String() + "\nContact List\n"
+			contactListStr += "B_ID\t| Address\t\t| K_ID\t\t\t\t\t\t| Distance\n"
 			for i := 0; i < len(buckets); i++ {
 				for contact := buckets[i].list.Front(); contact != nil; contact = contact.Next() {
 					contact := contact.Value.(Contact)
 					contact.CalcDistance(cli.node.rt.me.ID)
-					contactListStr +=  strconv.Itoa(i) + "\t| " + contact.Address + "\t| " + contact.ID.String() + "| " + contact.Distance.String() + "\n"
+					contactListStr +=  strconv.Itoa(i) + "\t| " + contact.Address + "\t| " + contact.ID.String() + "\t| " + contact.Distance.String() + "\n"
 				}
 			}
 			return contactListStr
